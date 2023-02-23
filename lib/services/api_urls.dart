0f35d7797c6be6f716b2a197fa33bc8029b3b179
Manish <manish.1webwiders@gmail.com>
2023-02-23 10:39:50 +0530

    --manish

--- lib/services/api_urls.dart
+++ lib/services/api_urls.dart
@@ -1,42 +1,8 @@
 class ApiUrls {
   static const baseUrl =
       'http://bluediamondresearch.com/WEB01/blind_dating/Api/';
-  static const String get_values = baseUrl + 'get_values';
-  static const String get_attributes = baseUrl + 'get_attributes';
-  static const String get_prominent = baseUrl + 'get_prominent';
-  static const String signup = baseUrl + 'signup';
-  static const String getUserDetails = baseUrl + 'get_user_profile?user_id=';
-  static const String login = baseUrl + 'login';
-  static const String verifyMobile = baseUrl + 'check_mobile_send_otp';
-  static const String privacy = baseUrl + 'privacy';
-  static const String contact = baseUrl + 'contact';
-  static const String terms = baseUrl + 'terms';
-  static const String forgot_password = baseUrl + 'forgot_password';
-  static const String checkEmail = baseUrl + 'checkEmail';
-  static const String change_password = baseUrl + 'change_password';
-  static const String edit_profile = baseUrl + 'edit_profile';
-  static const String deactiveMyAccount = baseUrl + 'deactiveMyAccount?user_id=';
-  static const String deleteMyAccount = baseUrl + 'deleteMyAccount?user_id=';
-static const String getMyMedia = baseUrl + 'getMyMedia?user_id=';
-static const String addMyMedia = baseUrl + 'addMyMedia';
-static const String deleteMyMedia = baseUrl + 'deleteMyMedia?';  //user_id=7&id=2
-static const String add_experiment = baseUrl + 'add_experiment';
-static const String get_experiment = baseUrl + 'get_experiment';
-static const String my_experiment = baseUrl + 'my_experiment';
-// static const String join_experiment = baseUrl + 'join_experiment';
-static const String allow_disallow_joiners = baseUrl + 'allow_disallow_joiners';
-static const String end_experiments = baseUrl + 'end_experiments';
-static const String get_default_quetions = baseUrl + 'get_default_quetions';
-static const String get_experiment_by_id = baseUrl + 'get_experiment_by_id';
-static const String join_experiment = baseUrl + 'join_experiment';
-  static const String updateDeviceToken = baseUrl + 'updateDeviceToken';
-  static const String GetNotification = baseUrl + 'GetNotification';
-  static const String reactOnExperimentAnswer = baseUrl + 'experiment_owner_reply';
+  // static const String get_values = baseUrl + 'get_values';
 
-// static const String terms = baseUrl + 'terms';
-// static const String terms = baseUrl + 'terms';
-// static const String terms = baseUrl + 'terms';
-// static const String terms = baseUrl + 'terms';
 
 
 }
